41c4e06aba6a7af4fb5aa73552fc4f1a4319102a
Hiru1003 <hirumiperera10@gmail.com>
2024-03-16 14:10:46 +0530

    feat: navigation

--- construction_app/lib/loadingPage2.dart
+++ construction_app/lib/loadingPage2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sealtech/components/theme.dart';
 import 'package:sealtech/signin.dart';
-
+import 'package:sealtech/signup.dart'; // Import SignInPage
 
 class LoadingPage2 extends StatelessWidget {
   @override
@@ -10,13 +10,17 @@ class LoadingPage2 extends StatelessWidget {
       backgroundColor: bgColor,
       body: GestureDetector(
         onTap: () {
+          // Navigate to SignInPage when tapped
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => SignInPage()),
+            MaterialPageRoute(builder: (context) => SignUpPage()),
           );
         },
         child: Center(
-          child: Image.asset('lib/images/logo-word-no-background.png', width: 160,),
+          child: Image.asset(
+            'lib/images/logo-word-no-background.png',
+            width: 160,
+          ),
         ),
       ),
     );
