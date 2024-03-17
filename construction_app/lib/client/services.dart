82014f645adba098b50ef30bc0d1dc85fbe73338
Hiru1003 <hirumiperera10@gmail.com>
2024-03-17 14:39:07 +0530

    feat: service page firestore

--- construction_app/lib/client/services.dart
+++ construction_app/lib/client/services.dart
@@ -1,66 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:sealtech/client/cardService.dart';
-import 'package:sealtech/client/product.dart';
 
-class Service extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text('Services'),
-          actions: [
-            IconButton(
-              icon: Image.asset('lib/images/logoIconBlack.png'),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Container(
-              child: Column(
+class ServicesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Services'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('services').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: (documents.length / 2).ceil(),
+            itemBuilder: (BuildContext context, int index) {
+              final startIndex = index * 2;
+              final endIndex = (index + 1) * 2;
+              return Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ServiceCard()),
-                        );
-                      },
-                        child : ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                      ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ],
-                  ),
+                  for (int i = startIndex;
+                      i < endIndex && i < documents.length;
+                      i++)
+                    Expanded(
+                      child: _buildServiceContainer(documents[i]),
+                    ),
                 ],
-              ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildServiceContainer(QueryDocumentSnapshot document) {
+    final Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    final title = data['name'] ?? ''; // Retrieve title from 'name' field
+    final price = data['price'] ?? ''; // Null check for price field
+    final imageUrl = data['imageUrl'] ?? '';
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            imageUrl,
+            height: 100,
+            width: 100,
+            errorBuilder: (context, error, stackTrace) {
+              return Icon(Icons.error);
+            },
+          ),
+          const SizedBox(height: 10), // Add vertical space
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-      );
+          const Text(
+            'Service',
+            style: TextStyle(
+              fontSize: 11,
+              color: Colors.grey,
+            ),
+          ),
+          Text(
+            price,
+            style: const TextStyle(
+              fontSize: 13,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
 }
